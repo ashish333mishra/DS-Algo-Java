2721b2db12bc775a34be5131d885deff045fd78d
Ashish Mishra <ashish.mishra@gslab.com>
2021-03-30 22:59:04 +0530

    Running sum solution

--- src/Topic_wise_questions/Arrays/Running_Sum_of_1D_Array.java
+++ src/Topic_wise_questions/Arrays/Running_Sum_of_1D_Array.java
@@ -1,10 +1,36 @@
 package Topic_wise_questions.Arrays;
 import java.util.*;
-import java.io.*;
 /*
  *Given an array we have to find running sum of the array
  *Definition of running sum runningSum[index] = SUM(array[0..index])
  */
-class Running_Sum_of_1D_Array{
-    
+class Running_Sum_of_1D_Array {
+    public static int[] runningSum(int[] nums) {
+        int sum = 0;
+        int index = 0;
+        for ( int i : nums) {
+            sum += i;
+            nums[index] = sum;
+            index++;
+        }
+        return nums;
+    }
+    public static void main(String[] args) {
+        System.out.println("Enter the size of array to find running Sum");
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        //Declaring array to store data
+        int[] dataArray = new int[size];
+        System.out.println("Enter the array elements");
+        for ( int i=0; i<size; i++) {
+            dataArray[i] = sc.nextInt();
+        }
+        dataArray = runningSum(dataArray);
+        printArray(dataArray);
+    }
+    public static void printArray(int[] data) {
+        for(int i : data) {
+            System.out.print(i + " ");
+        }
+    }
 }
