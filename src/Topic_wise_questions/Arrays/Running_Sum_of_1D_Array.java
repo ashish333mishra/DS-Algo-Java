374e3a4c0244a27c797c1f33840cd62a5d48c981
Ashish Mishra <ashish.mishra@gslab.com>
2021-03-30 21:08:41 +0530

    Initial commit

---
+++ src/Topic_wise_questions/Arrays/Running_Sum_of_1D_Array.java
@@ -0,0 +1,10 @@
+package Topic_wise_questions.Arrays;
+import java.util.*;
+import java.io.*;
+/*
+ *Given an array we have to find running sum of the array
+ *Definition of running sum runningSum[index] = SUM(array[0..index])
+ */
+class Running_Sum_of_1D_Array{
+    
+}
